6237bedf65c9ae4bdfa354ca7671d1e1822f5b3f
Rungwikrai <140418870+oakkmmmkk22@users.noreply.github.com>
2024-10-25 17:33:00 +0000

    ok

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './Shop.css';
 import axios from 'axios';
 function Item(props){
@@ -7,19 +7,20 @@ function Item(props){
         id: {props.id} <br/>
         name: {props.name}<br/>
         price: {props.price}<br/>
+        <button onClick={onDelete.bind(this,props.id)}>delete</button>
     </div>);
 }
 export default function Shop(){
+    const name_ref = useRef(null)
+    const price_ref = useRef(null)
+    const img_ref = useRef(null)
     const [products,setProducts]=useState([]);
-    const URL="https://ideal-winner-g4q54jrjg67rfw9jj-5000.app.github.dev";
+    const URL="https://ideal-winner-g4q54jrjg67rfw9jj-3001.app.github.dev";
     useEffect(()=>{
         axios.get(URL+'/api/products')
         .then(response=>{
             setProducts(response.data);
         })
-        .catch(error=>{
-            console.log("error");
-        });
     }
     ,[]);
     const [cart,setCart] = useState([]);
@@ -29,6 +30,28 @@ export default function Shop(){
     function resetCart(){
         setCart([]);
     }
+    function addProduct(){
+        const data={
+            name : name_ref.current.value,
+            price : price_ref.current.value,
+            img : img_ref.current.value
+        }
+        axios.post(URL+'/api/addproduct',data)
+        .then(response=>{
+            setProducts(response.data.products)
+        })
+        name_ref=null
+        price_ref=null
+        img_ref=null
+    }
+
+    function onDelete(id){
+        axios.delete(URL+"/api/product/"+id)
+        .then((response) => {
+            setProducts(response.data)
+        })
+    }
+
     const productsList=products.map(item=><Item {...item} callback={addCart}/>)
     const cartList=cart.map((item,index)=><li>{item.id} {item.name} {item.price}
         <button onClick={()=>{
@@ -40,6 +63,11 @@ export default function Shop(){
     for(let i=0;i<cart.length;i++) total+=cart[i].price;    
     return (
         <>
+            name : <input type="text" ref={name_ref} /><br />
+            price : <input type="text" ref={price_ref}/><br />
+            img : <input type="text" ref={img_ref}/><br />
+            <button onClick={addProduct}>add</button>
+
             <div className="grid-container">
                 {productsList}
             </div>
